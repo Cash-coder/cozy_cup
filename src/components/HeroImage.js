6c2e3612f361377dd86f00b90c95a2345e023636
smart <mail@>
2023-08-12 20:29:26 +0200

    working demo

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -3,23 +3,26 @@ import './ui/heroImage.css';
 function HeroImage(){
   return (
     <div className="hero-block two-columns-container">
-      <div>
-      <h1>Brew Amazing Coffee</h1>
-      <p className="Hero-p">
-        Learn to <strong> brew </strong> world-class coffee at home without confusing yourself.
-        <br/>
-        Click subscribe for free emails that will sharpen your coffee skills
-      </p>
-      <div>
-        <button className="hero-btn">Subscribe Free</button>
-        <button className="hero-btn insta">See Us On Instagram</button>
+      <div className="grid-item-0">
+        <div className="hero-image">
+          <div className="image-content"></div>
+        </div>
+      </div>
+      <div className="grid-item-1">
+        <h1>Brew Amazing Coffee</h1>
+        <p className="hero-p">
+          Learn to <strong> brew </strong> world-class coffee at home without confusing yourself.
+          <br/>
+          Click subscribe for free emails that will sharpen your coffee skills
+        </p>
+          <div className='buttons-container'>
+            <button className="hero-btn">Subscribe Free</button>
+            <button className="hero-btn insta">See Us On Instagram</button>
+          </div>
+        </div>
       </div>
 
-      <div className="hero-image">
-        <img src=" https://www.homegrounds.co/wp-content/uploads/2019/10/banner1-1.png" alt="Coffee hero image"/>
-      </div>
-      </div>
-    </div>
+
   )
 };
 
