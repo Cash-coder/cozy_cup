4d99b6edec09a6e685bd095575fb1284a5d64d5c
Cash-coder <vadymkozak3@gmail.com>
2023-08-13 12:49:32 +0200

    started dropdown menu

---
+++ src/components/SiteHeader.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './ui/siteHeader.css';
+
+
+export default function SiteHeader() {
+
+
+  return (
+    <div className='site-header'>
+      <div className="site-logo">
+        <img
+          src="https://static.vecteezy.com/system/resources/previews/000/585/220/large_2x/coffee-cup-logo-template-vector-icon-design.jpg"
+          alt="site logo by vecteezy"/>
+      </div>
+      <nav className='main-nav'>
+        <ul>
+          <li className="dropdown">
+            <a href="#">Tools</a>
+            <ul className="dropdown-content">
+              <li> <a href="#">Coffee Grinder</a> </li>
+              <li> <a href="#"> Coffee Compass</a> </li>
+            </ul>
+          </li>
+          <li><a href="#">Coffee Beans</a></li>
+          <li><a href="#">Gear</a></li>
+          <li><a href="#">Recipes</a></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
