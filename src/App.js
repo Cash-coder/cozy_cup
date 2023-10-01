415d8d2dadbb0942a49071b53205420fe533effc
Cash-coder <79537716+Cash-coder@users.noreply.github.com>
2023-10-01 20:13:28 +0200

    home styled

--- src/App.js
+++ src/App.js
@@ -8,30 +8,6 @@ import {whoWeAreData, tableArticleThumbrowData} from "./data/const";
 import TableArticleThumbrow from "./components/TableArticleThumbrow";
 import Footer from './components/Footer';
 
-// --- To Make later ---
-// Redux, url paths, context or similar
-
-// --- continue ---
-// page subscribe
-// popular posts
-// most read guides
-// page coffee flavour wheel. table of contents, accordeon faqs
-
-// --- technical debt ---
-// heroImage h1 and p getting cut below 1700px - width 97%
-// completar menús navegación
-
-
-// --- possibles ---
-// slide animation for mobile menu
-// what do you want to learn/brew/roast animation
-// icons home, rotate on hoover
-
-// --- resources ---
-  // https://www.homegrounds.co/
-
-
-
 export default function App() {
   return (
     <div className="App">
--- src/components/TableArticleThumbrow.js
+++ src/components/TableArticleThumbrow.js
@@ -4,15 +4,15 @@ export default function TableArticleThumbrow({ data }) {
   return (
     <div style={{ display:'flex', justifyContent: 'center'}}>
       <div className="table-article-thumbrow">
-      {data.map((item, index) => (
-        <div className='thumbrow-article' key={index}>
-          <a href={item.href || '#'}>
-            <img
-              src={item.imgSrc}
-              alt={item.imgAlt || item.title}
-            />
-            <h5>{item.title}</h5>
-          </a>
+        {data.map((item, index) => (
+          <div className='thumbrow-article' key={index}>
+            <a href={item.href || '#'}>
+              <img
+                src={item.imgSrc}
+                alt={item.imgAlt || item.title}
+              />
+              <h5>{item.title}</h5>
+            </a>
         </div>
       ))}
     </div>
