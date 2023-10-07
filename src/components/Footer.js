58edc0e4ade7334372c2cf230c7dbcce0b28a50b
smart <mail@>
2023-10-07 09:31:16 +0200

    footer desktop done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,25 +2,10 @@ import '../styles/footer.css';
 import {footerMenuItems} from '../data/const';
 
 export default function Footer () {
-const CoffeeCupPic = () => {
-  return <img src="../images/Coffee-Graphics.jpg" alt="Coffe cup"/>
-}
 
-  return (
-    <footer className="footer">
-      <div className="footer-wrapper">
-        <div className="footer-hero">
-          <div className="footer-contact">
-            <h2>Ready to elevate your coffee quality?</h2>
-            <div className="footer-buttons">
-              <a className="footer-btn" href="#">SIGN UP FOR FREE</a>
-              <a className="footer-btn" id="footer-btn-2" href="#">COFFEE GUIDES</a>
-            </div>
-          </div>
-          <img src={require('../images/coffee-cup.png')} alt="Coffe cup"/>
-        </div>
-        <div className="footer-nav">
-        <ul id="footer-widgets">
+  const FooterItems = () => (
+    <div className="footer-nav">
+      <ul id="footer-widgets">
         {footerMenuItems.map((item, index) =>
           <li key={index}>
             <a
@@ -29,6 +14,22 @@ const CoffeeCupPic = () => {
             </a>
           </li>)}
       </ul>
+    </div>
+  )
+
+  return (
+    <footer className="footer">
+      <FooterItems />
+      <div className="footer-wrapper">
+        <div className="footer-hero">
+          <div className="footer-contact">
+            <h4>Ready to elevate your coffee quality?</h4>
+            <div className="footer-buttons">
+              <a className="footer-btn" href="#">SIGN UP FOR FREE</a>
+              <a className="footer-btn" id="footer-btn-2" href="#">COFFEE GUIDES</a>
+            </div>
+          </div>
+          <img src={require('../images/coffee-cup.png')} alt="Coffe cup"/>
         </div>
       </div>
     </footer>
