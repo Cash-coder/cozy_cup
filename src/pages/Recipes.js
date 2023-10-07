821940ead3354fb9bbd4969a149c9f2e31fbde76
Cash-coder <79537716+Cash-coder@users.noreply.github.com>
2023-10-07 18:48:12 +0200

    added post banner

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,11 +1,21 @@
 import Header from '../components/Header';
+import PostBanner from "../components/PostBanner";
 import '../styles/recipes.css';
 
 export default function Recipes(){
   return (
     <>
       <Header />
-      <h2>Recipes</h2>
+      <PostBanner
+        img={{
+          src: "https://www.homegrounds.co/wp-content/uploads/2023/08/Recipes-homepgae-banner.webp",
+          alt: 'coffee recipes image'
+        }}
+        text={{
+          title: 'Coffee Recipes',
+          paragraph: 'Tired of the same old black coffee? Looking to liven up your coffee experience? We’ve put together some of the best coffee recipes on the internet here for you, from traditional espresso drinks to coffee ice cream. New recipes are added weekly!'
+        }}
+      />
     </>
   )
 }
